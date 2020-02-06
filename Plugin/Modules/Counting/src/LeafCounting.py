4b84a65c35fdb33bf090a8d888439c3b1fea2df0
stevenedixon <stevenedixon@outlook.com>
2020-02-06 16:04:05 +0000

    Initial Commit

--- Plugin/Modules/Counting/src/LeafCounting.py
+++ Plugin/Modules/Counting/src/LeafCounting.py
@@ -5,37 +5,35 @@ import wx
 import struct
 
 
-class LeafCounting():
-    property()  # Transient doesn't exist in Python, TODO: https://stackoverflow.com/questions/6313421/can-i-mark-variables-as-transient-so-they-wont-be-pickled
-    Dataset = struct  # Not sure if correct?
-    InternalListener: []
-    property
+class LeafCounting:
+    def __init__(self):
+        self.properties()  # Transient doesn't exist in Python, TODO: https://stackoverflow.com/questions/6313421/can-i-mark-variables-as-transient-so-they-wont-be-pickled
+        dataset = struct  # Not sure if correct?
+        internalListener: []
 
+    def currentDirectory(self):
+        os.getcwd()
+        pass
 
-def currentDirectory(args):
-    os.getcwd()
-    pass
-
-
-def properties():
-    ExperimentName: str = 'noname'
-    Parameters = struct('PatchSize', 19, ...,
-                        'RatioCurveWindowWidth', 20, ...,
-                        'LogPolarPoolingBands', 5, ...,
-                        'CartesianPoolingBands', 3, ...,
-                        'PoolingCallback', max, ...,
-                        'LogPolarNormalization', 0, ...,  # it means dynamic
-                        'CartesianNormalization', 100, ...,
-                        'CartesianFeatures', True, ...,
-                        'LogPolarFeatures', True, ...,
-                        'DictionarySize', 50, ...,
-                        'PoolingShifts', 4, ...,
-                        'SVMMatrixNormalization', 'zscore', ...,
-                        'Fast', False, ...,
-                        'CacheDirectory', print(currentDirectory), ...,
-                        'Autosave', True, ...,
-                        'SVR_OPT', struct('C', 1, 'gamma', 0.003, 'epsilon', 1), ...,
-                        'RF_OPT', struct('NumTrees', 100, 'MinLeafSize', 5), ...,
-                        'CategoricalField', 'Group', ...,
-                        'Fields', 'ProjectedLeafArea', ...,
-                        'TimeFeature', '')  # unix - delta
+    def properties(self):
+        experimentName: str = 'noname'
+        parameters = struct.pack('PatchSize', 19, ...,
+                                 'RatioCurveWindowWidth', 20, ...,
+                                 'LogPolarPoolingBands', 5, ...,
+                                 'CartesianPoolingBands', 3, ...,
+                                 'PoolingCallback', max, ...,
+                                 'LogPolarNormalization', 0, ...,  # it means dynamic
+                                 'CartesianNormalization', 100, ...,
+                                 'CartesianFeatures', True, ...,
+                                 'LogPolarFeatures', True, ...,
+                                 'DictionarySize', 50, ...,
+                                 'PoolingShifts', 4, ...,
+                                 'SVMMatrixNormalization', 'zscore', ...,
+                                 'Fast', False, ...,
+                                 'CacheDirectory', print(self.currentDirectory), ...,
+                                 'Autosave', True, ...,
+                                 'SVR_OPT', struct.pack('C', 1, 'gamma', 0.003, 'epsilon', 1), ...,
+                                 'RF_OPT', struct.pack('NumTrees', 100, 'MinLeafSize', 5), ...,
+                                 'CategoricalField', 'Group', ...,
+                                 'Fields', 'ProjectedLeafArea', ...,
+                                 'TimeFeature', '')  # unix - delta
