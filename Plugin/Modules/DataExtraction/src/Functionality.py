9940d3487eaecaa2c979a4013aaa2b7316941026
NotARectangle <milenabromm@hotmail.de>
2020-02-10 12:14:59 +0000

    initial commit

--- Plugin/Modules/DataExtraction/src/Functionality.py
+++ Plugin/Modules/DataExtraction/src/Functionality.py
@@ -1,6 +1,6 @@
 import csv
 
 with open('PhenotikiData.csv', newline='') as File:
-    reader = csv.reader(File, delimeter=' , ', quotechar=' \' ' quoting=csv.QUOTE_MINIMAL)
+    reader = csv.reader(File, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
     for row in reader:
         print(row)
