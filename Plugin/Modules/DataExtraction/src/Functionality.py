bbb7681c47c4a0e983855caf663eb19232bc92b3
stevenedixon <stevenedixon@outlook.com>
2020-02-13 15:39:52 +0000

    Date Extraction Initial

--- Plugin/Modules/DataExtraction/src/Functionality.py
+++ Plugin/Modules/DataExtraction/src/Functionality.py
@@ -1,9 +1,24 @@
 import csv
 
 data_array = []
+
 def open_file(filename):
     with open(filename, newline='') as File:
         reader = csv.reader(File, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
         for row in reader:
             data_array.append(row)
-    print(data_array)
+
+# [0] = Date
+# [1] = ID
+# [2] = Group
+# [3] = ProjectedLefArea
+# [4] = Diameter
+# [5] = Perimeter
+# [6] = Stockiness
+# [7] = Compactness
+# [8] = Hue
+# [9] = Count
+# [10] = RelativeRateChange
+# [11] = AbsoluteGrowthRate
+# [12] = RelativeGrowthRate
+def plot_graph(selection):
