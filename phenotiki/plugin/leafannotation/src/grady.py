e14b63aaead1d5a454b09b9a73f9a02abef99544
Matt <chomikmaster10@wp.pl>
2020-03-12 17:57:38 +0000

    Grady-binary image

--- phenotiki/plugin/leafannotation/src/grady.py
+++ phenotiki/plugin/leafannotation/src/grady.py
@@ -14,7 +14,7 @@ img = cv2.imread(
     '-00.png ', -1)
 alpha = img[:, :, 2]  # extract it
 binary = ~alpha  # invert b/w
-#cv2.imshow("Gay", img)
+# cv2.imshow("Gay", img)
 cv2.waitKey()
 
 # blank_image = np.zeros((128, 128, 3), np.uint8)
@@ -29,14 +29,26 @@ data += np.random.normal(loc=0, scale=sigma, size=data.shape)
 data = rescale_intensity(data, in_range=(-sigma, 1 + sigma),
                          out_range=(-1, 1))
 
+data1 = skimage.img_as_float64(alpha, force_copy=False)
+
+sigma = 0.35
+data1 += np.random.normal(loc=0, scale=sigma, size=data1.shape)
+data1 = rescale_intensity(data1, in_range=(-sigma, 1 + sigma),
+                         out_range=(-1, 1))
+
 # The range of the binary image spans over (-1, 1).
 # We choose the hottest and the coldest pixels as markers.
-markers = np.zeros(data.shape, dtype=np.uint)
-markers[data < -0.95] = 1
-markers[data > 0.95] = 2
+
+markers = np.zeros(data1.shape, dtype=np.uint)
+markers[data1 < -0.95] = 1
+markers[data1 > 0.95] = 2
+
+#markers1 = np.zeros(data1.shape, dtype=np.uint)
+#markers1[data1 < -0.95] = 1
+#markers1[data1 > 0.95] = 2
 
 # Run random walker algorithm
-labels = random_walker(data, markers, beta=10, mode='cg_mg')
+labels = random_walker(data1, markers, mode='cg_mg')
 
 # Plot results
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 3.2),
