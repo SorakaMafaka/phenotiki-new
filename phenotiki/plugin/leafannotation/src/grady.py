59deab7d7c9097f13cd8723b8736bc296332a8d1
Matt <chomikmaster10@wp.pl>
2020-03-12 17:14:48 +0000

    Grady

--- phenotiki/plugin/leafannotation/src/grady.py
+++ phenotiki/plugin/leafannotation/src/grady.py
@@ -1,13 +1,29 @@
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+from skimage import io
 from skimage.segmentation import random_walker
 from skimage.data import binary_blobs
 from skimage.exposure import rescale_intensity
 import skimage
+import cv2
 
 # Generate noisy synthetic data
-data = skimage.img_as_float(binary_blobs(length=128, seed=1))
+img = cv2.imread(
+    'C:\\Users\\SorakaMafaka\\PycharmProjects\\Phenotiki\\phenotiki\\plugin\\leafannotation\\src\\IMG_2013-09-28_08' \
+    '-00.png ', -1)
+alpha = img[:, :, 2]  # extract it
+binary = ~alpha  # invert b/w
+#cv2.imshow("Gay", img)
+cv2.waitKey()
+
+# blank_image = np.zeros((128, 128, 3), np.uint8)
+# blank_image[:,0:128//2] = (255,0,0)
+# blank_image[:,0:128//2:128] = (0,255,0)
+# skimage.io.imread
+
+data = skimage.img_as_float64(img, force_copy=False)
+
 sigma = 0.35
 data += np.random.normal(loc=0, scale=sigma, size=data.shape)
 data = rescale_intensity(data, in_range=(-sigma, 1 + sigma),
@@ -20,7 +36,7 @@ markers[data < -0.95] = 1
 markers[data > 0.95] = 2
 
 # Run random walker algorithm
-labels = random_walker(data, markers, beta=10, mode='bf')
+labels = random_walker(data, markers, beta=10, mode='cg_mg')
 
 # Plot results
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 3.2),
@@ -37,3 +53,4 @@ ax3.set_title('Segmentation')
 
 fig.tight_layout()
 plt.show()
+print(data)
