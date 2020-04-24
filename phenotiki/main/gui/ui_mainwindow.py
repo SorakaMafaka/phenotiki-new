9778c652ca2fc42dcef0775848932a329698b514
stevenedixon <stevenedixon@outlook.com>
2020-04-24 16:18:58 +0100

    comments

--- phenotiki/main/gui/ui_mainwindow.py
+++ phenotiki/main/gui/ui_mainwindow.py
@@ -9,7 +9,8 @@ from phenotiki.main.src.import_functionality import *
 from phenotiki.plugin.leafannotation.gui.LA_Tab import LA_Tab
 from phenotiki.plugin.tray.gui.PT_Tab import PT_Tab
 
-#Author(s):
+
+# Author(s):
 #   Converted from "Phenotiki - True phenotyping-in-a-box solution"
 #   by Steven Dixon, Milena Bromm, Mateusz Glowacki
 #   Original "Phenotiki - True phenotyping-in-a-box solution"
@@ -26,7 +27,7 @@ class Ui_MainWindow(object):
         if MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.setEnabled(True)
-        MainWindow.resize(1024, 740)# change this line to make resizable
+        MainWindow.resize(1024, 740)  # change this line to make resizable
         sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -51,12 +52,12 @@ class Ui_MainWindow(object):
         sizePolicy.setHeightForWidth(self.tabMain.sizePolicy().hasHeightForWidth())
         self.tabMain.setSizePolicy(sizePolicy)
 
+        # About Button
         self.main_btnAbout = QPushButton(self.tabMain)
         self.main_btnAbout.setObjectName(u"main_btnAbout")
         self.main_btnAbout.setGeometry(QRect(900, 350, 111, 51))
-        ##About button image
 
-        ##
+        # Header Label
         self.main_lblHeader = QLabel(self.tabMain)
         self.main_lblHeader.setObjectName(u"main_lblHeader")
         self.main_lblHeader.setGeometry(QRect(10, 280, 301, 81))
@@ -65,6 +66,8 @@ class Ui_MainWindow(object):
         self.main_lblHeader.setFont(font)
         self.main_lblHeader.setTextFormat(Qt.PlainText)
         self.main_lblHeader.setTextInteractionFlags(Qt.NoTextInteraction)
+
+        # Sub Header Label
         self.main_lblSubHeader = QLabel(self.tabMain)
         self.main_lblSubHeader.setObjectName(u"main_lblSubHeader")
         self.main_lblSubHeader.setGeometry(QRect(10, 350, 471, 41))
@@ -74,17 +77,24 @@ class Ui_MainWindow(object):
         self.main_lblSubHeader.setFocusPolicy(Qt.NoFocus)
         self.main_lblSubHeader.setStyleSheet(u"color: green")
         self.main_lblSubHeader.setWordWrap(False)
+
+        # Logo Label
         self.main_lblLogo = QLabel(self.tabMain)
         self.main_lblLogo.setObjectName(u"main_lblLogo")
         self.main_lblLogo.setGeometry(QRect(0, 0, 1021, 261))
         self.main_lblLogo.setPixmap(QPixmap(u"../gui/img/Logo.png"))
+
+        # Group Box News
         self.main_gbxNews = QGroupBox(self.tabMain)
         self.main_gbxNews.setObjectName(u"main_gbxNews")
         self.main_gbxNews.setGeometry(QRect(10, 400, 1001, 291))
+
+        # News Label
         self.main_lblNews = QLabel(self.main_gbxNews)
         self.main_lblNews.setObjectName(u"main_lblNews")
         self.main_lblNews.setGeometry(QRect(10, 40, 281, 21))
         self.tabWidget.addTab(self.tabMain, "")
+
         # Add Module UI Tabs
         self.PotTrayTab = PT_Tab(self.tabWidget)
         self.LeafAnnotationTab = LA_Tab(self.tabWidget)
@@ -98,23 +108,22 @@ class Ui_MainWindow(object):
         self.main_btnAbout.clicked.connect(self.show_about)
         QMetaObject.connectSlotsByName(MainWindow)
 
-
     def show_about(self):
         msg = QMessageBox()
         msg.setWindowTitle("About")
         # Program Title
         msg.setText("<h1> Phenotiki </h1> <h2> True Phenotyping-in-a-box solution </h2> ")
-        #Main Text of the About Page
+        # Main Text of the About Page
         # Read Changelog in Hyperlink colour as well as "Freepik" "Plainicon" "www.flaticon.com"
-        msg.setInformativeText('Current Version 1.2 \n \nRead CHANGELOG\n  \nM.Minervini[1], M.V.Giuffrida[1], S,Tsaftaris[2] '
-                               '\nIMT Advanced Studies of Lucca[1] \nUniversity of Edinburgh[2] \n '
-                               '\nSoftware License reported in the user\'s manual. \n \nIcons made by Freepik, Plainicon'
-                               'from www.flaticon.com is licensed by CC BY 3.0')
-        #Add Authors University Logos
+        msg.setInformativeText(
+            'Current Version 1.2 \n \nRead CHANGELOG\n  \nM.Minervini[1], M.V.Giuffrida[1], S,Tsaftaris[2] '
+            '\nIMT Advanced Studies of Lucca[1] \nUniversity of Edinburgh[2] \n '
+            '\nSoftware License reported in the user\'s manual. \n \nIcons made by Freepik, Plainicon'
+            'from www.flaticon.com is licensed by CC BY 3.0')
+        # Add Authors University Logos
         msg.setIconPixmap(QPixmap(u"../gui/img/Test.PNG"))
 
-        x= msg.exec_()
-
+        x = msg.exec_()
 
     # add main tab items
     def retranslateUi(self, MainWindow):
